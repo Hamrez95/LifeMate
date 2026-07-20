1e62546c0ad8a78d6ef6536647a2fb4610e69fe9
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-07-20 16:25:27 +0330

    Keep the CareMate shell smoke test free of polling side effects

--- caremate/test/widget_test.dart
+++ caremate/test/widget_test.dart
@@ -10,7 +10,7 @@ void main() {
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (_) => LocaleProvider(),
-        child: const CareMateApp(),
+        child: const CareMateApp(home: SizedBox.shrink()),
       ),
     );
 
