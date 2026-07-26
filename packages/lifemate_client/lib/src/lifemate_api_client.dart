833ddcb85ef37c40a3b0dbab017ea77a1b1a1e64
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-07-27 01:58:54 +0330

    Generate secure idempotency request ids

--- packages/lifemate_client/lib/src/lifemate_api_client.dart
+++ packages/lifemate_client/lib/src/lifemate_api_client.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:http/http.dart' as http;
 
@@ -33,6 +34,16 @@ class LifeMateApiClient {
   final Uri _baseUri;
   final AccessTokenProvider _accessToken;
   final http.Client _http;
+
+  static String createClientRequestId() {
+    final bytes = List<int>.generate(16, (_) => Random.secure().nextInt(256));
+    bytes[6] = (bytes[6] & 0x0f) | 0x40;
+    bytes[8] = (bytes[8] & 0x3f) | 0x80;
+    final hex = bytes.map((value) => value.toRadixString(16).padLeft(2, '0')).join();
+    return '${hex.substring(0, 8)}-${hex.substring(8, 12)}-'
+        '${hex.substring(12, 16)}-${hex.substring(16, 20)}-'
+        '${hex.substring(20)}';
+  }
 
   Future<void> bootstrapUser({
     required String? displayName,
