bd9f1f4193f278c98a41f083710055e174ec35f5
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-03 23:50:31 +0330

    feat(pairing): export QR payload contract

--- packages/lifemate_client/lib/lifemate_client.dart
+++ packages/lifemate_client/lib/lifemate_client.dart
@@ -1,6 +1,7 @@
 library lifemate_client;
 
 export 'src/app_config.dart';
+export 'src/care_pairing_qr.dart';
 export 'src/lifemate_api_client.dart';
 export 'src/lifemate_auth.dart';
 export 'src/lifemate_bootstrap.dart';
