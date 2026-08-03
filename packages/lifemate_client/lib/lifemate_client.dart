411bc549434c01095beb6fc1df787cafa2938208
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-04 01:35:49 +0330

    feat(auth): export shared feature flags

--- packages/lifemate_client/lib/lifemate_client.dart
+++ packages/lifemate_client/lib/lifemate_client.dart
@@ -2,6 +2,7 @@ library lifemate_client;
 
 export 'src/app_config.dart';
 export 'src/care_pairing_qr.dart';
+export 'src/feature_flags.dart';
 export 'src/lifemate_api_client.dart';
 export 'src/lifemate_auth.dart';
 export 'src/lifemate_bootstrap.dart';
