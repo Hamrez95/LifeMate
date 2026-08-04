ab478e13b1f1b623a142c67c03493f540479ed63
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-04 20:36:43 +0330

    test: require activation-gated women calendar navigation

--- wellmate/test/women_calendar_navigation_test.dart
+++ wellmate/test/women_calendar_navigation_test.dart
@@ -7,30 +7,44 @@ import 'package:wellmate/main.dart';
 import 'package:wellmate/providers/settings_provider.dart';
 
 void main() {
-  testWidgets('women calendar navigation is present and Persian labelled', (
+  testWidgets('women calendar navigation is hidden before activation', (
     tester,
   ) async {
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => LocaleProvider()),
-          ChangeNotifierProvider(create: (_) => SettingsProvider()),
-        ],
-        child: WellMateApp(
-          home: Scaffold(
-            bottomNavigationBar: WellMateBottomNav(
-              currentIndex: 4,
-              womenCalendarEnabled: true,
-              onTap: (_) {},
-            ),
-          ),
-        ),
-      ),
-    );
+    await tester.pumpWidget(_navHarness(womenCalendarEnabled: false));
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const ValueKey('wellmate-nav-3')), findsNothing);
+    expect(find.bySemanticsLabel('تقویم بانوان'), findsNothing);
+    expect(find.byKey(const ValueKey('wellmate-nav-4')), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+
+  testWidgets('women calendar navigation appears after activation', (
+    tester,
+  ) async {
+    await tester.pumpWidget(_navHarness(womenCalendarEnabled: true));
     await tester.pumpAndSettle();
 
     expect(find.byKey(const ValueKey('wellmate-nav-3')), findsOneWidget);
     expect(find.bySemanticsLabel('تقویم بانوان'), findsOneWidget);
     expect(tester.takeException(), isNull);
   });
+}
+
+Widget _navHarness({required bool womenCalendarEnabled}) {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => LocaleProvider()),
+      ChangeNotifierProvider(create: (_) => SettingsProvider()),
+    ],
+    child: WellMateApp(
+      home: Scaffold(
+        bottomNavigationBar: WellMateBottomNav(
+          currentIndex: 4,
+          womenCalendarEnabled: womenCalendarEnabled,
+          onTap: (_) {},
+        ),
+      ),
+    ),
+  );
 }
