821001d4cd40d6f697f4c8dfe795e6be791ce140
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-06 01:08:26 +0330

    chore(shared): export auth and women companion surfaces together

--- packages/lifemate_client/lib/lifemate_client.dart
+++ packages/lifemate_client/lib/lifemate_client.dart
@@ -8,6 +8,7 @@ export 'src/lifemate_api_client.dart';
 export 'src/lifemate_auth.dart';
 export 'src/lifemate_bootstrap.dart';
 export 'src/lifemate_edit_api.dart';
+export 'src/lifemate_experience_gate.dart';
 export 'src/nullable_women_calendar_estimate.dart';
 export 'src/profile_avatar.dart';
 export 'src/reminder_lead_time.dart';
