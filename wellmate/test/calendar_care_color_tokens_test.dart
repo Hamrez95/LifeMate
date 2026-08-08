ae451a54451187247a9cc043619cda0545b8fd89
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-08 15:06:13 +0330

    test: assert care type colors remain distinct

--- wellmate/test/calendar_care_color_tokens_test.dart
+++ wellmate/test/calendar_care_color_tokens_test.dart
@@ -3,9 +3,11 @@ import 'package:wellmate/core/theme/app_style.dart';
 import 'package:wellmate/screens/calendar/calendar_utils.dart';
 
 void main() {
-  test('calendar uses semantic care item color tokens', () {
+  test('calendar uses distinct semantic care item color tokens', () {
     expect(CalendarUtils.getColorForType('med'), AppColors.careMedication);
     expect(CalendarUtils.getColorForType('appointment'), AppColors.careVisit);
     expect(CalendarUtils.getColorForType('injection'), AppColors.careInjection);
+    expect(AppColors.careInjection, isNot(AppColors.careVisit));
+    expect(AppColors.careInjection, isNot(AppColors.careMedication));
   });
 }
