fba455772fb6865932014cb201ef690d69b5028a
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-12 23:52:20 +0330

    test(wellmate): keep fertility safety assertion viewport independent

--- wellmate/test/women_cycle_character_card_test.dart
+++ wellmate/test/women_cycle_character_card_test.dart
@@ -90,7 +90,6 @@ void main() {
 
     expect(find.text('روزهای باروری'), findsNothing);
     expect(find.text('تخمک‌گذاری'), findsNothing);
-    expect(find.byIcon(Icons.info_outline_rounded), findsOneWidget);
     expect(tester.takeException(), isNull);
   });
 }
