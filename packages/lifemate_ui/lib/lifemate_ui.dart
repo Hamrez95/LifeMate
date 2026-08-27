0b71504e7441b2ec55f70ebe125c3631d8c0db79
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-27 09:27:31 +0330

    feat(onboarding): export specialized onboarding controls

--- packages/lifemate_ui/lib/lifemate_ui.dart
+++ packages/lifemate_ui/lib/lifemate_ui.dart
@@ -1,6 +1,7 @@
 library lifemate_ui;
 
 export 'src/onboarding_components.dart';
+export 'src/onboarding_specialized.dart';
 export 'src/onboarding_theme.dart';
 export 'src/profile_theme.dart';
 export 'src/shared_editable_profile_screen.dart';
