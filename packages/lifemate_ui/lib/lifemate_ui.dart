0e036fabac986170b359d1b443ab16b62c3e11c1
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-27 14:53:57 +0330

    refactor(profile): export privacy-enabled shared profile

--- packages/lifemate_ui/lib/lifemate_ui.dart
+++ packages/lifemate_ui/lib/lifemate_ui.dart
@@ -9,4 +9,5 @@ export 'src/shared_account_onboarding.dart' hide LifeMateAccountOnboardingGate;
 export 'src/shared_auth_experience.dart';
 export 'src/shared_editable_profile_screen.dart';
 export 'src/shared_legal_privacy.dart';
-export 'src/shared_profile_screen.dart';
+export 'src/shared_profile_screen.dart' hide LifeMateSharedProfileScreen;
+export 'src/shared_profile_with_privacy.dart';
