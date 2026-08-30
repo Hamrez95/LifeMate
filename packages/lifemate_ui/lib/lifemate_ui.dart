9ebe047c7a823b1316aea71a7128a79ec140d5c2
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-31 00:33:58 +0330

    feat(i18n): export localization foundation (#674)

--- packages/lifemate_ui/lib/lifemate_ui.dart
+++ packages/lifemate_ui/lib/lifemate_ui.dart
@@ -2,6 +2,7 @@ library lifemate_ui;
 
 export 'src/companion_care_guidance.dart';
 export 'src/demographics_experience.dart';
+export 'src/localization.dart';
 export 'src/onboarding_components.dart';
 export 'src/onboarding_specialized.dart';
 export 'src/onboarding_theme.dart';
