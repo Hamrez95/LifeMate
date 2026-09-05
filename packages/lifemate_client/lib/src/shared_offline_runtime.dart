3dafb41eacabd2b6404925406efe175159a8b083
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 11:15:02 +0330

    chore(offline): keep runtime conditional export focused

--- packages/lifemate_client/lib/src/shared_offline_runtime.dart
+++ packages/lifemate_client/lib/src/shared_offline_runtime.dart
@@ -1,4 +1,2 @@
-export 'offline_namespace_local_extension_web.dart'
-    if (dart.library.io) 'offline_namespace_local_extension.dart';
 export 'shared_offline_runtime_web.dart'
     if (dart.library.io) 'shared_offline_runtime_native.dart';
