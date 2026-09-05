c0993285bce0c5fce76d421eb5d86886363a4af5
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 04:26:05 +0330

    feat(offline): export projection reconciler

--- packages/lifemate_core/lib/lifemate_core.dart
+++ packages/lifemate_core/lib/lifemate_core.dart
@@ -4,5 +4,6 @@ export 'src/local_health_reminder_registry.dart';
 export 'src/local_health_store.dart';
 export 'src/local_mutation_outbox.dart';
 export 'src/local_mutation_replay.dart';
+export 'src/local_projection_reconciler.dart';
 export 'src/local_reminder_scheduler.dart';
 export 'src/local_sync_checkpoint.dart';
