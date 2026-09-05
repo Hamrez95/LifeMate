60757571aa5ec965bf5b0e982bfb48cf8d4fb2cc
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 08:29:10 +0330

    fix(web): isolate native lifemate_core storage surface

--- packages/lifemate_core/lib/lifemate_core.dart
+++ packages/lifemate_core/lib/lifemate_core.dart
@@ -1,11 +1,4 @@
 library lifemate_core;
 
-export 'src/affected_reminder_reconciler.dart';
-export 'src/local_conflict_policy.dart';
-export 'src/local_health_reminder_registry.dart';
-export 'src/local_health_store.dart';
-export 'src/local_mutation_outbox.dart';
-export 'src/local_mutation_replay.dart';
-export 'src/local_projection_reconciler.dart';
-export 'src/local_reminder_scheduler.dart';
-export 'src/local_sync_checkpoint.dart';
+export 'lifemate_core_web.dart'
+    if (dart.library.io) 'lifemate_core_native.dart';
