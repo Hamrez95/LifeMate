0e7289b487d310b4d853ef2fa148bc0158dbafab
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 04:31:06 +0330

    style(offline): apply dart format to conflict policy

--- packages/lifemate_core/lib/src/local_conflict_policy.dart
+++ packages/lifemate_core/lib/src/local_conflict_policy.dart
@@ -103,8 +103,7 @@ abstract final class LifeMateConflictPolicy {
       LifeMateConflictDomain.womenHealthCycle => _resolveWomenCycle(context),
       LifeMateConflictDomain.pregnancyDating => _resolvePregnancyDating(context),
       LifeMateConflictDomain.observation => _resolveObservation(context),
-      LifeMateConflictDomain.sharedAuthorization =>
-        _resolveSharedAuthorization(context),
+      LifeMateConflictDomain.sharedAuthorization => _resolveSharedAuthorization(context),
     };
   }
 
