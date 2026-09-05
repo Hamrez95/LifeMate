bc795656eb758a85f0d68e66e91265898450f459
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 04:05:04 +0330

    fix(offline): condition native sqlite exports for web

--- packages/lifemate_client/lib/lifemate_client.dart
+++ packages/lifemate_client/lib/lifemate_client.dart
@@ -42,7 +42,8 @@ export 'src/lifemate_experience_gate.dart';
 export 'src/locale_digit_input_formatter.dart';
 export 'src/medication_schedule_api.dart';
 export 'src/medication_sleep_schedule_api.dart';
-export 'src/mutation_replay_transport.dart';
+export 'src/native_offline_api_stub.dart'
+    if (dart.library.io) 'src/mutation_replay_transport.dart';
 export 'src/notification_intelligence.dart';
 export 'src/nullable_women_calendar_estimate.dart';
 export 'src/offline_mutation_queue.dart';
@@ -62,7 +63,8 @@ export 'src/runtime_locale.dart';
 export 'src/secure_session_storage.dart';
 export 'src/session_gate.dart' hide LifeMateSessionGate;
 export 'src/session_gate_secure.dart';
-export 'src/shared_offline_runtime.dart';
+export 'src/shared_offline_runtime_stub.dart'
+    if (dart.library.io) 'src/shared_offline_runtime.dart';
 export 'src/support_conversations_api.dart';
 export 'src/women_calendar.dart';
 export 'src/women_companion_api.dart';
