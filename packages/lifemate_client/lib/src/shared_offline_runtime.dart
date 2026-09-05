836d2547b285fe44543b28291e60a74d37ca20a2
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-05 11:16:44 +0330

    fix(offline): bind local namespace mapping in runtime facade

--- packages/lifemate_client/lib/src/shared_offline_runtime.dart
+++ packages/lifemate_client/lib/src/shared_offline_runtime.dart
@@ -1,2 +1,15 @@
+import 'package:lifemate_core/lifemate_core.dart';
+
+import 'shared_offline_runtime_web.dart'
+    if (dart.library.io) 'shared_offline_runtime_native.dart';
+
 export 'shared_offline_runtime_web.dart'
     if (dart.library.io) 'shared_offline_runtime_native.dart';
+
+extension LifeMateOfflineNamespaceLocalMapping on LifeMateOfflineNamespace {
+  LifeMateLocalNamespace toLocalNamespace() => LifeMateLocalNamespace(
+    environmentId: environmentId,
+    accountId: accountId,
+    personId: personId,
+  );
+}
