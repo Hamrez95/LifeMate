0666037ec0bf0a28aca64d2fc503cdc5ed60cfa7
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-11 12:57:11 +0330

    test(circle): settle capacity feedback animation (#1092)

--- lifemate/test/camp_companion_selection_test.dart
+++ lifemate/test/camp_companion_selection_test.dart
@@ -71,7 +71,7 @@ void main() {
     expect(find.text('1 of 1 selected'), findsOneWidget);
 
     await tester.tap(find.text('Sam'));
-    await tester.pump();
+    await tester.pumpAndSettle();
     expect(
       find.text('You can show up to 1 companions in Camp.'),
       findsOneWidget,
