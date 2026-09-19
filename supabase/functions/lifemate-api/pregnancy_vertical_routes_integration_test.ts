f27a9a3fc14bb905181cf19901e7362a6674364f
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-19 20:45:06 +0330

    style(cocoon): format pregnancy integration imports

--- supabase/functions/lifemate-api/pregnancy_vertical_routes_integration_test.ts
+++ supabase/functions/lifemate-api/pregnancy_vertical_routes_integration_test.ts
@@ -1,7 +1,4 @@
-import {
-  assertEquals,
-  assertRejects,
-} from "jsr:@std/assert@1.0.14";
+import { assertEquals, assertRejects } from "jsr:@std/assert@1.0.14";
 import postgres from "postgres";
 import { closeLifeMateSqlClientsForTest } from "./database_client.ts";
 import { createPregnancyCaptureRouteHandler } from "./pregnancy_capture.ts";
